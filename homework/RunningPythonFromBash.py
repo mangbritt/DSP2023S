a99a663644410f4d65ee11f0f4b816c91177ffc3
Britt Mang <mang.britt@gmail.com>
2023-02-20 04:27:19 -0600

    Uploading homework

---
+++ homework/RunningPythonFromBash.py
@@ -0,0 +1,3 @@
+#! \usr\bin\env python
+
+print("This is Python version 3.5.2 \n\nPython is the best language for String Interpretation! \n\n!noitalupinaM gnirtS rof egaugnal tseb eht si nohtyP \n\n!otlpnmgit o gunlte h inhy \n\npYTHON IS THE BEST LANGUAGE FOR sTRING MANIPULATION! \n\n\nThe sentence 'Python is the best language for String Manipulation!' contains \n4 'a' letters, and \n0 'A' letters! \n\nPython \nis \nthe \nbest \nlanguage \nfor \nString \nmanipulation! \n\nPYTHON \nIS \nTHE \nBEST \nLANGUAGE \nFOR \nSTRING \nMANIPULATION!")
